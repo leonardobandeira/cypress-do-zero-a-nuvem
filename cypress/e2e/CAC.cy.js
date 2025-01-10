a482699079d8c32c698003ef1f19ae7bd23ae122
Leonardo Lucena <lucena.lbd@gmail.com>
2025-01-09 22:48:10 -0300

    test: criando teste básico de preenchimento e submissão

--- cypress/e2e/CAC.cy.js
+++ cypress/e2e/CAC.cy.js
@@ -6,4 +6,15 @@ describe('Central de Atendimento ao Cliente', () => {
   it('verificar o titulo da aplicacao', () => {
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
   })
+
+  it('preencher os campos obrigatórios e envia o formulário', () =>{
+    cy.get('#firstName').type('Pedro')
+    cy.get('#lastName').type('Lima')
+    cy.get('#email').type('teste@mail.com')
+    cy.get('#open-text-area').type('Obrigado!')
+
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.success').should('be.visible')
+  })
 })
