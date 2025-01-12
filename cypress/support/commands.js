e3d455a49ae501fef5c5375283121e1c94f2895c
Leonardo Lucena <lucena.lbd@gmail.com>
2025-01-11 23:05:57 -0300

    test: melhorando o comando personalizado

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,15 +3,29 @@ Cypress.Commands.add('fillMandatoryFieldsAndSubmit', () => {
     cy.get('#lastName').type('Pedro P')
     cy.get('#email').type('pedropedro@pedro.p')
     cy.get('#open-text-area').type('Teste automatico!')
-    
+
     cy.get('button[type="submit"]').click()
 })
 
-Cypress.Commands.add('fillMandatoryFieldsAndSubmit', data => {
+Cypress.Commands.add('fillMandatoryFieldsAndSubmitWithData', data => {
     cy.get('#firstName').type(data.nome)
     cy.get('#lastName').type(data.sobrenome)
     cy.get('#email').type(data.email)
     cy.get('#open-text-area').type(data.texto)
-    
+
+    cy.get('button[type="submit"]').click()
+})
+
+Cypress.Commands.add('fillMandatoryFieldsAndSubmitWithData2', (data = {
+    nome: 'Pedro Pedro',
+    sobrenome: 'Pedro P',
+    email: 'pedropedro@pedro.p',
+    texto: 'Teste automatizado!'
+}) => {
+    cy.get('#firstName').type(data.nome)
+    cy.get('#lastName').type(data.sobrenome)
+    cy.get('#email').type(data.email)
+    cy.get('#open-text-area').type(data.texto)
+
     cy.get('button[type="submit"]').click()
 })
